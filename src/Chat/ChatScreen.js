f5778c363406be339cf0de2cf407e8db3c485885
thewebunity <5060vishalkumar@gmail.com>
2023-04-04 17:01:30 +0530

    chat

--- src/Chat/ChatScreen.js
+++ src/Chat/ChatScreen.js
@@ -1,68 +1,183 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useState, useEffect, useCallback} from 'react';
-import {GiftedChat} from 'react-native-gifted-chat';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableWithoutFeedback,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  ToastAndroid,
+  Pressable,
+  Alert,
+  Button,
+} from 'react-native';
+import {FlagIcon, PaperAirplaneIcon} from 'react-native-heroicons/solid';
 import database from '@react-native-firebase/database';
+import Colors from '../Utitlies/Colors';
+import Family from '../Utitlies/Family';
 
-const ChatScreen = () => {
-  const [messages, setMessages] = useState([]);
+const Chatscreen = () => {
+  const [Input_messsage, setInput_messsage] = useState('');
+  const [Message, setMessage] = useState([]);
+  const [userId, setuserId] = useState(1);
+  const [Typing, setTyping] = useState(false);
 
-  useEffect(() => {
-    // setMessages([
-    //   {
-    //     _id: '1909c917-f3a2-4b60-b3b9-7f05aadf11cb',
-    //     text: 'hello vivek',
-    //     createdAt: new Date(),
-    //     user: {
-    //       _id: 1,
-    //     },
-    //   },
-    // ]);
+  const sendMessage = async () => {
+    if (Input_messsage.length > 0) {
+      const newReference = database().ref('/users/215-209/chat').push();
+      newReference
+        .set({
+          _id: '2',
+          text: Input_messsage,
+          createdAt: new Date().getTime(),
+          sender: '1',
+        })
+        .then(() => {
+          ToastAndroid.show('Mesage Sent', ToastAndroid.SHORT);
+        });
+      setInput_messsage('');
+      typingFalse();
+    } else {
+      Alert.alert('Please type Message');
+    }
+  };
+
+  const typingFalse = () => {
+    database().ref('/users/215-209').update({
+      typing: false,
+    });
+  };
+
+  const UpdateStatus = text => {
+    setInput_messsage(text);
+    if (text.length > 0) {
+      database().ref('/users/215-209/typing').update({
+        typing: true,
+      });
+    } else {
+      database().ref('/users/215-209/typing').update({
+        typing: false,
+      });
+    }
+  };
+
+  const getStatus = () => {
     database()
-      .ref('/users/215-209')
+      .ref('/users/215-209/typing')
+      .on('value', snapshot => {
+        if (snapshot.val() == null) {
+          setTyping(false);
+        } else if (snapshot.val().typing == true) {
+          setTyping(true);
+        } else {
+          setTyping(false);
+        }
+      });
+  };
+
+  // const sendMessageUser = async () => {
+  //   const newReference = database().ref('/users/215-209/chat').push();
+  //   newReference.set({
+  //     _id: '1',
+  //     text: 'from another side',
+  //     createdAt: new Date().getTime(),
+  //     sender: '2',
+  //   });
+  //   setInput_messsage('');
+  // };
+
+  const getChat = async () => {
+    database()
+      .ref('/users/215-209/chat')
       .on('value', snapshot => {
         let Arr = [];
         snapshot.forEach(value => {
-          console.log(value);
+          const {_id, text, createdAt, sender} = value.val();
           Arr.push({
-            _id: value._id,
-            text: 'text',
-            createdAt: new Date(),
-            user: {
-              _id: 1,
-            },
+            _id: _id,
+            text: text,
+            createdAt: createdAt,
+            sender: sender,
           });
         });
-        setMessages(Arr);
+        setMessage(Arr);
       });
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages =>
-      GiftedChat.append(previousMessages, messages),
-    );
-    const {_id, createdAt, text, user} = messages[0];
-    const newReference = database().ref('/users/215-209').push();
-    newReference.set({
-      _id: _id,
-      text: text,
-      createdAt: createdAt,
-      user: user,
-    });
+  };
+  useEffect(() => {
+    getChat();
+    getStatus();
   }, []);
 
   return (
     <View style={{flex: 1}}>
-      <GiftedChat
-        messages={messages}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-      />
+      <View style={{marginVertical: 10}}>
+        <Text style={{textAlign: 'center', fontFamily: Family.Medium}}>
+          {Typing ? 'typing.....' : ''}
+        </Text>
+        <FlatList
+          data={Message}
+          style={{marginBottom: 70}}
+          renderItem={({item}) => {
+            return (
+              <Pressable>
+                <View style={{marginTop: 0, width: '95%', alignSelf: 'center'}}>
+                  <View
+                    style={{
+                      backgroundColor:
+                        item.sender == userId ? Colors.primary : Colors.light,
+                      maxWidth: Dimensions.get('screen').width * 0.8,
+                      paddingVertical: 5,
+                      paddingHorizontal: 15,
+                      alignSelf:
+                        item.sender == userId ? 'flex-end' : 'flex-start',
+                      borderRadius: 8,
+                      borderBottomLeftRadius: item.sender === userId ? 8 : 0,
+                      borderBottomRightRadius: item.sender === userId ? 0 : 8,
+                      marginVertical: 5,
+                    }}>
+                    <Text
+                      style={{
+                        padding: 5,
+                        color:
+                          item.sender == userId ? Colors.light : Colors.dark,
+                        fontFamily: 'Poppins-Regular',
+                      }}>
+                      {item.text}
+                    </Text>
+                  </View>
+                </View>
+              </Pressable>
+            );
+          }}
+        />
+      </View>
+      {/* <Button title="Send Message" onPress={sendMessageUser} /> */}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          justifyContent: 'space-around',
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingHorizontal: 15,
+          paddingVertical: 7,
+          backgroundColor: '#fafafa',
+        }}>
+        <TextInput
+          placeholder="Type your message here"
+          style={{flex: 1, fontFamily: 'Poppins-Medium'}}
+          onChangeText={text => UpdateStatus(text)}
+          value={Input_messsage}
+          keyboardType="default"
+        />
+        <TouchableOpacity onPress={sendMessage}>
+          <PaperAirplaneIcon color="#97667C" size={24} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default ChatScreen;
-
-const styles = StyleSheet.create({});
+export default Chatscreen;
